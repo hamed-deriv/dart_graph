77ac7c41e709b7acda062d6e3f11d6106097b074
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-05-11 17:14:44 +0800

    add basic traversal

--- bin/dart_graph.dart
+++ bin/dart_graph.dart
@@ -1,3 +1,18 @@
 import 'package:dart_graph/dart_graph.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  print('Depth first print:');
+  depthFirstPrint(graph: graph, start: a);
+
+  print('\nBreadth first print:');
+  breadthFirstPrint(graph: graph, start: a);
+}
+
+Node a = Node(value: 'A', neighbors: [b, c]);
+Node b = Node(value: 'B', neighbors: [d]);
+Node c = Node(value: 'C', neighbors: [e]);
+Node d = Node(value: 'D', neighbors: [f]);
+Node e = Node(value: 'E', neighbors: []);
+Node f = Node(value: 'F', neighbors: []);
+
+Graph graph = Graph(nodes: [a, b, c, d, e, f]);
--- lib/dart_graph.dart
+++ lib/dart_graph.dart
@@ -1,1 +1,42 @@
-class Graph {}
+class Graph {
+  final List<Node>? nodes;
+
+  Graph({this.nodes});
+}
+
+class Node<T> {
+  final T value;
+  final List<Node<T>> neighbors;
+
+  Node({required this.value, required this.neighbors});
+}
+
+void breadthFirstPrint<T>({required Graph graph, required Node<T> start}) {
+  final Set<Node<T>> visited = <Node<T>>{};
+
+  visited.add(start);
+
+  while (visited.isNotEmpty) {
+    final Node<T> node = visited.first;
+    visited.remove(node);
+
+    print(node.value);
+
+    visited.addAll(node.neighbors);
+  }
+}
+
+void depthFirstPrint<T>({required Graph graph, required Node<T> start}) {
+  final Set<Node<T>> visited = <Node<T>>{};
+
+  visited.add(start);
+
+  while (visited.isNotEmpty) {
+    final Node<T> node = visited.last;
+    visited.remove(node);
+
+    print(node.value);
+
+    visited.addAll(node.neighbors);
+  }
+}
